d939032ff447ec27a366c3c4e68238c428b86c58
kavitalearns <kavitabansal_01@yahoo.co.in>
2020-11-29 17:52:48 -0600

    completed section-3

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+   styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+     recipes: Recipe[] =[
+       new Recipe('A test recipe', 'This is simply a test', 'https://storage.needpix.com/rsynced_images/food-1459693_1280.jpg'),
+        new Recipe('A test recipe', 'This is simply a test', 'https://storage.needpix.com/rsynced_images/food-1459693_1280.jpg')
+     ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
